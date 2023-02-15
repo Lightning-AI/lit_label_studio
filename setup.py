eba73c42d7d7cc524ffc85a81bfccf7a0bff639b
Jirka <jirka.borovec@seznam.cz>
2023-02-15 18:50:57 +0100

    adjusting setup

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
+import os
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
+
+_PATH_ROOT = os.path.dirname(__file__)
+
+
+def _load_requirements(path_dir: str = _PATH_ROOT, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
+
 
 setup(
     name="lit_label_studio",
@@ -9,11 +19,7 @@ setup(
     author="Robert S Lee",
     author_email="sangkyulee@gmail.com",
     url="https://github.com/Lightning-AI/lit_label_studio",
-    install_requires=[
-        "lightning",
-        "virtualenv",
-        "lit_bashwork @ https://github.com/Lightning-AI/lit_bashwork/archive/refs/tags/0.0.3.tar.gz",
-    ],
+    install_requires=_load_requirements(),
     packages=find_packages(),
     include_package_data=True,
     package_data={"": ["nginx-8080.conf"]},
